167dc9541d281d54a3352c911d0e5e3c12a2d488
tsterbak <sterbak-it@outlook.com>
2016-08-07 14:16:34 +0200

    C4.5 trees + foggy trees
    Random forest

--- C45.py
+++ C45.py
@@ -8,6 +8,8 @@ from sklearn.datasets import load_iris
 from sklearn.cross_validation import train_test_split
 from sklearn.metrics.classification import accuracy_score
 from sklearn.tree.tree import DecisionTreeClassifier
+from sklearn.datasets.base import load_digits
+import time
 
 class decisionnode:
     def __init__(self,feature=-1,value=None,results=None,tb=None,fb=None):
@@ -17,13 +19,95 @@ class decisionnode:
         self.tb=tb
         self.fb=fb
 
+class foggy_decision_tree:
+    '''
+    C4.5 classification tree with normally distributed splits at prediction time
+    '''
+    def __init__(self, max_depth=4, var=0.1):
+        self.max_depth = max_depth
+        self._current_depth = 0
+        self.var = var
+    
+    def fit(self,X,y):
+        self._tree = self.build_tree(X,y)
+        return self
+    
+    def predict_one(self, sample, tree):
+        if tree.results != None:
+            return tree.results
+        else:
+            v = sample[tree.feature]
+            branch = None
+            value = np.random.normal(loc=tree.value, scale=self.var)
+            if isinstance(v,int) or isinstance(v,float):
+                if v >= value: 
+                    branch = tree.tb
+                else: 
+                    branch = tree.fb
+            else:
+                if v == value: 
+                    branch = tree.tb
+                else: 
+                    branch = tree.fb
+            return self.predict_one(sample, branch)
+    
+    def predict(self,X):
+        y_pred = []
+        for i in range(X.shape[0]):
+            pred = self.predict_one(X[i], self._tree)
+            y_pred.append(pred)
+        return np.array(y_pred)
+    
+    def divide_set(self,samples,targets, feature, value):
+        lower_set = samples[samples[:,feature] > value]
+        lower_set_target = targets[samples[:,feature] > value]
+        higher_set = samples[samples[:,feature] <= value]
+        higher_set_target = targets[samples[:,feature] <= value]
+        return lower_set,lower_set_target, higher_set, higher_set_target
+    
+    def entropy(self,y):
+        from math import log
+        log2 = lambda x:log(x)/log(2)  
+        possible_labels = list(set(y))
+        ent=0.0
+        for label in possible_labels:
+            p = float(np.count_nonzero(y == label))/len(y)
+            ent = ent-p*log2(p)
+        return ent
+    
+    def build_tree(self,X,y):
+        current_score = self.entropy(y)
+        best_gain = 0.0
+        best_criteria = None
+        best_sets = None
+        best_labels = None
+        for feature in range(0,X.shape[1]):
+            for value in np.unique(X[:,feature]):
+                lower_set,lower_set_target, higher_set, higher_set_target = self.divide_set(X, y, feature, value)
+                p = float(lower_set.shape[0])/X.shape[0]
+                gain = current_score - p*self.entropy(lower_set_target) - (1-p)*self.entropy(higher_set_target)
+                if gain > best_gain and lower_set.shape[0] > 0 and higher_set.shape[0] > 0:
+                    best_gain = gain
+                    best_criteria = (feature,value)
+                    best_sets = (lower_set,higher_set)
+                    best_labels = (lower_set_target,higher_set_target)
+        if best_gain > 0 and self._current_depth < self.max_depth:
+            self._current_depth += 1
+            trueBranch = self.build_tree(best_sets[0], best_labels[0])
+            falseBranch = self.build_tree(best_sets[1], best_labels[1])
+            return decisionnode(feature=best_criteria[0],value=best_criteria[1],tb=trueBranch,fb=falseBranch)
+        else:
+            values, counts = np.unique(y,return_counts=True)
+            ind=np.argmax(counts)
+            return decisionnode(results=values[ind])
+
 class decision_tree_c45:
     '''
-    classification tree C4.5
+    C4.5 classification tree
     '''
-    def __init__(self,max_depth=4):
+    def __init__(self, max_depth=4):
         self.max_depth = max_depth
-        self.current_depth = 0
+        self._current_depth = 0
     
     def fit(self,X,y):
         self._tree = self.build_tree(X,y)
@@ -63,12 +147,12 @@ class decision_tree_c45:
     
     def entropy(self,y):
         from math import log
-        log2=lambda x:log(x)/log(2)  
+        log2 = lambda x:log(x)/log(2)  
         possible_labels = list(set(y))
         ent=0.0
         for label in possible_labels:
-            p=float(np.count_nonzero(y == label))/len(y)
-            ent=ent-p*log2(p)
+            p = float(np.count_nonzero(y == label))/len(y)
+            ent = ent-p*log2(p)
         return ent
     
     def build_tree(self,X,y):
@@ -87,8 +171,8 @@ class decision_tree_c45:
                     best_criteria = (feature,value)
                     best_sets = (lower_set,higher_set)
                     best_labels = (lower_set_target,higher_set_target)
-        if best_gain > 0 and self.current_depth < self.max_depth:
-            self.current_depth += 1
+        if best_gain > 0 and self._current_depth < self.max_depth:
+            self._current_depth += 1
             trueBranch = self.build_tree(best_sets[0], best_labels[0])
             falseBranch = self.build_tree(best_sets[1], best_labels[1])
             return decisionnode(feature=best_criteria[0],value=best_criteria[1],tb=trueBranch,fb=falseBranch)
@@ -96,7 +180,7 @@ class decision_tree_c45:
             values, counts = np.unique(y,return_counts=True)
             ind=np.argmax(counts)
             return decisionnode(results=values[ind])
-
+    
 def printtree(tree, indent=''):
     if tree.results!=None:
         print(str(tree.results))
@@ -110,31 +194,29 @@ def printtree(tree, indent=''):
 
 
 if __name__ == "__main__":
-    iris = load_iris()
+    iris = load_digits(n_class=2)
     X = iris.data
     y = iris.target
     
     X_train, X_test, y_train, y_test = train_test_split(X,y,train_size=0.66, random_state=2016)
     
+    t0 = time.time()
     tree = decision_tree_c45(max_depth=2).fit(X_train,y_train)
-    
+    # tree = foggy_decision_tree(max_depth=2, var=0.1).fit(X_train,y_train)
     y_pred = tree.predict(X_test)
-    #print(y_pred)
-    #print(y_test)
-     
+    print("Time taken: %0.3f" %(time.time() - t0))
+    
     print("")
     score = accuracy_score(y_test, y_pred)
     print("Score: %0.3f" %score)
     print("")
     
     # printtree(tree._tree,indent='')
-
-    sklear_tree = DecisionTreeClassifier(max_depth=2).fit(X_train, y_train)
+    t0 = time.time()
+    sklear_tree = DecisionTreeClassifier(max_depth=2, random_state=2016).fit(X_train, y_train)
     y_pred = sklear_tree.predict(X_test)
-    #print(y_pred)
-    #print(y_test)
-     
-    print("")
+    print("Time taken: %0.3f" %(time.time() - t0))
+    
     score = accuracy_score(y_test, y_pred)
     print("Score: %0.3f" %score)
     print("")
---
+++ forest.py
@@ -0,0 +1,117 @@
+'''
+Created on 07.08.2016
+
+@author: Tobias
+'''
+import numpy as np
+from C45 import decision_tree_c45, foggy_decision_tree
+from sklearn.datasets.base import load_digits
+from sklearn.cross_validation import train_test_split
+from sklearn.ensemble.forest import RandomForestClassifier
+from sklearn.metrics.classification import accuracy_score
+import time
+
+class random_forest:
+    '''
+    standard random forest
+    '''
+    def __init__(self,n_estimators=10, max_depth=4, bootstrap=True, sample_ratio=1.0, seed=2016):
+        self.n_estimators = n_estimators
+        self.max_depth = max_depth
+        self.bootstrap = bootstrap
+        self.sample_ratio = sample_ratio
+        self.seed = 2016
+    
+    def fit(self,X,y):
+        self._trees = []
+        np.random.seed(self.seed)
+        for i in range(self.n_estimators):
+            ind = np.random.choice(X.shape[0], int(X.shape[0]*self.sample_ratio), replace=self.bootstrap)
+            X_temp = X[ind,:]
+            y_temp = y[ind]
+            tree = decision_tree_c45(max_depth=self.max_depth).fit(X_temp,y_temp)
+            self._trees.append(tree)
+        return self    
+        
+    def predict(self,X):
+        predictions = []
+        for tree in self._trees:
+            y_pred = tree.predict(X)
+            predictions.append(y_pred)
+        y_out = self.voting(predictions)
+        return y_out
+    
+    def voting(self, predictions):
+        def most_freq(x):
+            values, counts = np.unique(x,return_counts=True)
+            ind=np.argmax(counts)
+            return values[ind]
+        y_out = [most_freq(x) for x in np.array(predictions).T]
+        return np.array(y_out)
+
+class foggy_forest:
+    '''
+    forest of randomized foggy trees
+    '''
+    def __init__(self,n_estimators=10, max_depth=4, bootstrap=True, sample_ratio=1.0, var=0.5, seed=2016):
+        self.n_estimators = n_estimators
+        self.max_depth = max_depth
+        self.bootstrap = bootstrap
+        self.sample_ratio = sample_ratio
+        self.seed = 2016
+        self.var = var
+    
+    def fit(self,X,y):
+        self._trees = []
+        np.random.seed(self.seed)
+        for i in range(self.n_estimators):
+            ind = np.random.choice(X.shape[0], int(X.shape[0]*self.sample_ratio), replace=self.bootstrap)
+            X_temp = X[ind,:]
+            y_temp = y[ind]
+            tree = foggy_decision_tree(max_depth=self.max_depth, var=self.var).fit(X_temp,y_temp)
+            self._trees.append(tree)
+        return self    
+        
+    def predict(self,X):
+        predictions = []
+        for tree in self._trees:
+            y_pred = tree.predict(X)
+            predictions.append(y_pred)
+        y_out = self.voting(predictions)
+        return y_out
+    
+    def voting(self, predictions):
+        def most_freq(x):
+            values, counts = np.unique(x,return_counts=True)
+            ind=np.argmax(counts)
+            return values[ind]
+        y_out = [most_freq(x) for x in np.array(predictions).T]
+        return np.array(y_out)
+
+if __name__ == "__main__":
+    digits = load_digits(n_class=2)
+    X = digits.data
+    y = digits.target
+    
+    X_train, X_test, y_train, y_test = train_test_split(X,y,train_size=0.5, random_state=2016)
+    
+    t0 = time.time()
+    forest = random_forest(max_depth=4, n_estimators=20).fit(X_train,y_train)
+    y_pred = forest.predict(X_test)
+    print("Time taken: %0.3f" %(time.time() - t0))
+    print(y_pred)
+    
+    print("")
+    score = accuracy_score(y_test, y_pred)
+    print("Score: %0.3f" %score)
+    print("")
+    
+    # printtree(tree._tree,indent='')
+    t0 = time.time()
+    sklearn_forest = RandomForestClassifier(max_depth=4, n_estimators=20, random_state=2016).fit(X_train, y_train)
+    y_pred = sklearn_forest.predict(X_test)
+    print("Time taken: %0.3f" %(time.time() - t0))
+    print(y_pred)
+    score = accuracy_score(y_test, y_pred)
+    print("Score: %0.3f" %score)
+    print("")
